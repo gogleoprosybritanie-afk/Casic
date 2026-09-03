412a7063d00a23055faf8b355d9e466647983b3a
gogleoprosybritanie-afk <gogleoprosybritanie@gmail.com>
2026-09-03 14:34:21 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-TOKEN = "8389798404:AAFAF5q-g2Y2-KzitYKFgns6lHr4Z9bZ7tI" # токен бота
+TOKEN = "8389798404-TOKEN" # токен бота
 
-CHANNEL_ID = "-1004351839859" # ид канала со ставками
+CHANNEL_ID = "-" # ид канала со ставками
 
-ADMIN_CHANNEL_ID = "-1004428662174" # ид канала для заявок (для админа)
+ADMIN_CHANNEL_ID = "ID" # ид канала для заявок (для админа)
 
 ADMIN_ID = 8226881959 # ид админа
 
-CHANNEL_LINK = "https://t.me/Wromlybet" # ссылка на канал со ставками
+CHANNEL_LINK = "https://t.me/" # ссылка на канал со ставками
